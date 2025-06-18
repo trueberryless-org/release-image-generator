a4dbb471bfd14be1dc0c6fb5bdde2e224446be60
Felix Schneider <99918022+trueberryless@users.noreply.github.com>
2025-06-18 18:11:21 +0200

    feat: image formats (png, jpg, webp)

--- app/ReleaseImageGenerator.Domain/Implementations/ImageGeneratorOptions.cs
+++ app/ReleaseImageGenerator.Domain/Implementations/ImageGeneratorOptions.cs
@@ -1,6 +1,6 @@
 ﻿namespace ReleaseImageGenerator.Domain.Implementations;
 
-public record ImageGeneratorOptions(string? text, int width, int height, SupportedFontFamily fontFamily, SupportedFontWeight fontWeight, string? primaryColor);
+public record ImageGeneratorOptions(string? text, int width, int height, SupportedFontFamily fontFamily, SupportedFontWeight fontWeight, string? primaryColor, SupportedImageFormat imageFormat);
 
 public enum SupportedFontFamily
 {
@@ -24,4 +24,12 @@ public enum SupportedFontWeight
     bold,
     medium,
     light
+}
+
+public enum SupportedImageFormat
+{
+    jpeg,
+    jpg,
+    png,
+    webp
 }
